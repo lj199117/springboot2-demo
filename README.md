d3841b5747a43dee25588c7c14498d568442dbe7
李锦 <goldliclass@163.com>
2019-03-26 16:22:34 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # springboot2-demo
+
+1.集成swagger
+2.自定义log4j2
+3.集成mybatis
--- springboot2-demo-center/src/main/java/com/demo/ApplicationDemo.java
+++ springboot2-demo-center/src/main/java/com/demo/ApplicationDemo.java
@@ -4,13 +4,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.battcn.swagger.annotation.EnableSwagger2Doc;
 @EnableSwagger2Doc
 @SpringBootApplication
-public class ApplicationDemo {
+public class ApplicationDemo extends SpringBootServletInitializer {
 	private static final Logger LOGGER = LogManager.getLogger(ApplicationDemo.class);
     public static void main(String[] args) {
         SpringApplication.run(ApplicationDemo.class, args);
     }
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
 }
